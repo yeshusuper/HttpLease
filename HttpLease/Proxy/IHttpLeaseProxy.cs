52ce7c4ad1bf46c009b59ce3ebe4b0d1c19b4c48
wulf <yeshusuper@qq.com>
2015-03-25 13:36:34 +0800

    漏了提交的代码

---
+++ HttpLease/Proxy/IHttpLeaseProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpLease.Proxy
+{
+    internal interface IHttpLeaseProxy<T>
+        where T : class
+    {
+        T Client { get; }
+    }
+
+    
+}
