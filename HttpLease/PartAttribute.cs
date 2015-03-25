baf8c8c9000685970798007e1d9db04a2a7319ad
Lam <yeshusuper@qq.com>
2015-03-25 20:54:38 +0800

    上传文件开发中

---
+++ HttpLease/PartAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpLease
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class PartAttribute : BaseParameterAttribute
+    {
+        /// <summary>
+        /// 默认true
+        /// </summary>
+        public bool IsFile { get; set; }
+
+        public PartAttribute()
+        {
+            IsFile = true;
+        }
+    }
+}
