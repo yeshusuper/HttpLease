57fc6c788a8aa5c9e3b967d1e69e80f1fdd58cb4
wulf <yeshusuper@qq.com>
2015-08-05 17:01:30 +0800

    增加自定义ContentType

--- HttpLease/EnctypeAttribute.cs
+++ HttpLease/EnctypeAttribute.cs
@@ -12,4 +12,41 @@ namespace HttpLease
         internal abstract bool DefaultEncodeKey { get; }
         internal abstract bool DefaultEncodeValue { get; }
     }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class CustomEnctypeAttribute : EnctypeAttribute
+    {
+        /// <summary>
+        /// 默认为true
+        /// </summary>
+        public bool IsEncodeKey { get; set; }
+        /// <summary>
+        /// 默认为true
+        /// </summary>
+        public bool IsEncodeValue { get; set; }
+
+        public CustomEnctypeAttribute(string contentType)
+        {
+            IsEncodeKey = true;
+            IsEncodeValue = true;
+            _ContentType = contentType;
+        }
+
+        private string _ContentType;
+
+        internal override string ContentType
+        {
+            get { return _ContentType; }
+        }
+
+        internal override bool DefaultEncodeKey
+        {
+            get { return IsEncodeKey; }
+        }
+
+        internal override bool DefaultEncodeValue
+        {
+            get { return IsEncodeValue; }
+        }
+    }
 }
