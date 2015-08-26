3f03a5db85a323b8e227c71a3a009c4eb1da882b
wulf <yeshusuper@qq.com>
2015-08-26 11:37:30 +0800

    百度ip测试

--- HttpLease.Tests/ITaobao.cs
+++ HttpLease.Tests/ITaobao.cs
@@ -14,6 +14,11 @@ namespace HttpLease.Tests
         [HttpGet]
         [Url("http://ip.taobao.com/service/getIpInfo.php")]
         TaoBaoIPResponse Get(string ip);
+
+        [Timeout(5000)]
+        [HttpGet]
+        [Url("http://apis.baidu.com/apistore/iplookupservice/iplookup")]
+        BaiduIPResponse BaiduIP([Header(Name = "apikey")]string apiKey, string ip);
     }
     public class TaoBaoIPResponse
     {
@@ -32,5 +37,32 @@ namespace HttpLease.Tests
         public int Code { get; set; }
         [JsonProperty(PropertyName = "data")]
         public IpInfo Data { get; set; }
+    }
+
+    public class BaiduIPResponse
+    {
+        public class Ret
+        {
+            [JsonProperty(PropertyName = "ip")]
+            public string IP { get; set; }
+            [JsonProperty(PropertyName = "country")]
+            public string Country { get; set; }
+            [JsonProperty(PropertyName = "province")]
+            public string Province { get; set; }
+            [JsonProperty(PropertyName = "city")]
+            public string City { get; set; }
+            [JsonProperty(PropertyName = "district")]
+            public string District { get; set; }
+            [JsonProperty(PropertyName = "carrier")]
+            public string Carrier { get; set; }
+
+        }
+
+        [JsonProperty(PropertyName = "errNum")]
+        public int ErrNum { get; set; }
+        [JsonProperty(PropertyName = "errMsg")]
+        public string ErrMsg { get; set; }
+        [JsonProperty(PropertyName = "retData")]
+        public Ret RetData { get; set; }
     }
 }
